1fadb6361034048685c842b24fd489b5f598b9a1
周祥 <zx22962@ly.com>
2017-06-15 21:17:41 +0800

    0.0.5

--- client/index.js
+++ client/index.js
@@ -2,8 +2,8 @@
 
 var EventEmiter   = require('events');
 var net           = require('net');
-var RedisParser = require('redis-parser');
-// var {formatReply} = require('../lib');
+var RedisParser   = require('redis-parser');
+var {formatReply} = require('../lib');
 
 class RedisClient extends EventEmiter{
 
@@ -12,26 +12,40 @@ class RedisClient extends EventEmiter{
 		super();
 
 		this._connected = false;
-		this._cmdQueue  = [];
+		this._cmds  = [];
 
 		// init connection
 		this.redisCnt = net.connect({host, port}, () => {
 			this._connected = true;
-			this.sendCommonQueue();
+			
+			// 缓存的cmd
+			this._cmds.forEach(cmd => { this.redisCnt.write(`${cmd.cmd}\r\n`) });
 		})
-		// options 
-		// this.redisCnt.setKeepAlive(true, 0);
-		// this.redisCnt.setNoDelay(true);
-		// this.redisCnt.setTimeout(10000, () => {  });
-		this.redisCnt.on('error', this.connectError);
-		this.redisCnt.on('close', () => { this._connected = false });
-		this.redisCnt.on('data', ret => { this._parser.execute(ret); });
 
 		// init parser
 		this._parser = new RedisParser({
 			returnError: this.returnError.bind(this),
 		    returnReply: this.returnReply.bind(this)
 		});
+
+		// options 
+		// this.redisCnt.setKeepAlive(true, 0);
+		// this.redisCnt.setNoDelay(true);
+		// this.redisCnt.setTimeout(10000, () => {  });
+
+		this.redisCnt.on('error', this.connectError);
+		this.redisCnt.on('close', this.redisClose);
+		this.redisCnt.on('data', this.getData(this));
+	}
+	// get data
+	getData (client){
+		return function (ret) {
+			client._parser.execute(ret);
+		}
+	}
+	// redis close
+	redisClose (){
+		this._connected = false
 	}
 	// connect error
 	connectError (err){
@@ -39,22 +53,21 @@ class RedisClient extends EventEmiter{
 	}
 	// returnError
 	returnError (err) {
-		this._cmdQueue.shift();
+		this._cmds.shift();
 	}
 	// push reply
 	returnReply (reply){
-		var cmd = this._cmdQueue.shift();
+		var cmd = this._cmds.shift();
 		cmd.cb && cmd.cb(reply);
 	}
 	// push cmd
 	commond (cmd, cb) {
-		this._cmdQueue.push({cmd, cb});
-	}
-	// send cmd queue
-	sendCommonQueue (){
-		for(var i=0;i<this._cmdQueue.length;i++){
-			var cmd = this._cmdQueue[i];
-			this.redisCnt.write(`${cmd.cmd}\r\n`);
+
+		var readyState = this.redisCnt.readyState;
+		this._cmds.push({cmd, cb});
+
+		if(readyState == 'open'){ // 如果已连接直接发送
+			this.redisCnt.write(`${cmd}\r\n`);
 		}
 	}
 }
